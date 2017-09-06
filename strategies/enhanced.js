c9a84e57cae4d1176caec626db6a385b096a98ee
Matthew Walls <matthew.walls@guardian.co.uk>
2017-09-06 10:47:22 +0100

    Add strategies, pull out of src directory

---
+++ strategies/enhanced.js
@@ -0,0 +1,143 @@
+
+"use strict";
+
+import Config from '../config';
+
+const MINUTES = 60000;
+const HOURS = MINUTES*60;
+const DAYS = HOURS*24;
+
+export class EnhancedPickStrategy {
+
+    // The Strategy determines the order to present questions to the user.
+
+    constructor(queue, horizon = 2*DAYS) {
+
+        this.queue = queue;
+        this.horizon = horizon;
+        this.dateFactory = Date;
+        this.abunai = new Set();
+        this.wrongtracker = [];
+
+        if(queue == undefined){
+            throw new Error("Undefined queue");
+        }
+
+        var now = this.dateFactory.now();
+
+        var lowestBucketWithItems = Math.min.apply(
+            Math, this.queue.map((i2)=>i2.bucket)
+        );
+
+        var recentItems = this.queue.filter(
+            (i) => (now - i.firstSeen) <= this.horizon && i.eligibleForPromotion()
+        );
+
+        var lowBucketItems = this.queue.filter(
+            (i) => i.bucket === lowestBucketWithItems && i.eligibleForPromotion()
+        );
+
+        this.abunai = new Set(
+            recentItems.concat(lowBucketItems)
+        );
+
+    }
+
+    right(item) {
+        // some strategies might change behavior depending on
+        // how the user is doing in real-time, so we need to
+        // expose callbacks for when the user gets items right
+
+        // if it was in the wrongtracker, move it to the next step
+        // of the wrongtracker
+
+        this.wrongtracker.filter((i)=>i.item == item).forEach((i)=> {
+            i.step += 1;
+            i.left = 1 + Math.pow(i.step, 2);
+        });
+
+        // we don't want any that that got over step 3
+        this.wrongtracker = this.wrongtracker.filter((i)=>i.step <= 3);
+        
+    }
+
+    wrong(item) {
+
+        // some strategies might change behavior depending on
+        // how the user is doing in real-time, so we need to
+        // expose callbacks for when the user gets items wrong
+
+        this.wrongtracker = this.wrongtracker.filter((i)=>i.item != item);
+        this.wrongtracker.push({"step":1, "left": 3, "item": item});
+        
+    }
+
+    pick() {        
+        
+        if(this.queue.length == 0){
+            return undefined;
+        }
+
+        var randomItem = function(lst) {
+            return lst[Math.floor(Math.random()*lst.length)];
+        }
+
+        // decrement all the wrongtracker things
+
+        this.wrongtracker.forEach((i)=>{ i.left -= 1 });
+        
+        // priority 1: wrong items that reached 0 in the wrongtracker countdown
+
+        const wrongoes = this.wrongtracker.filter((i) => i.left <= 0);
+
+        if(wrongoes.length > 0){
+            const item = wrongoes[0];
+            console.log("Picking wrongo with character " + item.item.character);
+            delete this.wrongtracker[item];
+            return item.item;
+        }
+
+        // priority 2: abunai items
+
+        if(this.abunai.size != 0){
+            console.log("Picking random Abunai item");            
+            const item = randomItem(Array.from(this.abunai));
+            this.abunai.delete(item);
+            return item
+        }
+        
+        // If we didn't land on an abunai item, show a stale one
+        // (or just a random item if we've run out of stales)
+
+        var staleItems = this.queue.filter(
+            (i) => i.eligibleForPromotion()
+        );
+
+        var newestFirst = function(a, b) {
+            return a.firstSeen - b.firstSeen;
+        }
+
+        staleItems.sort(newestFirst);
+
+        if(staleItems.length > 0){
+            console.log("Picking last stale item");
+            return staleItems[0];
+        } else {
+            console.log("Picking random item");
+            return randomItem(this.queue)
+        }
+
+    }
+
+    isFinished() {
+        return this.queue.length == 0;
+    }    
+
+    amountLeft() {
+        return Infinity;
+    }
+
+
+}
+
+module.exports = EnhancedPickStrategy;
---
+++ strategies/single.js
@@ -0,0 +1,115 @@
+
+"use strict";
+
+import Config from '../config';
+
+const MINUTES = 60000;
+const HOURS = MINUTES*60;
+const DAYS = HOURS*24;
+
+export class SingleLevelPickStrategy {
+
+    // The Strategy determines the order to present questions to the user.
+
+    constructor(queue) {
+
+        if(queue == undefined){
+            throw new Error("Undefined queue");
+        };        
+
+        // duplicate the queue
+        this.original = queue;
+        this.queue = queue.map((i)=>i);
+        
+        this.dateFactory = Date;
+        this.abunai = [
+            { chance: 0.5, items: new Set()},
+            { chance: 0.25, items: new Set()},
+            { chance: 0.1, items: new Set()}
+        ];
+
+        
+    }
+
+    right(item) {
+        // some strategies might change behavior depending on
+        // how the user is doing in real-time, so we need to
+        // expose callbacks for when the user gets items right
+
+        for(let i in this.abunai){
+            var next = +i+1;
+            if(this.abunai[i].items.has(item)){
+                
+                this.abunai[i].items.delete(item);
+
+                if(next < this.abunai.length){
+                    this.abunai[next].items.add(item);
+                }
+                
+                return;
+                
+            }
+        }
+        
+    }
+
+    wrong(item) {
+        // some strategies might change behavior depending on
+        // how the user is doing in real-time, so we need to
+        // expose callbacks for when the user gets items wrong
+
+        for(let abunai of this.abunai){
+            abunai.items.delete(item);
+        }
+
+        this.abunai[0].items.add(item);
+        
+    }
+
+    pick() {
+
+        // start over again if we exhaust the queue
+        
+        if(this.queue.length == 0){
+            console.log("exhausted queue. Rebuilding");
+            this.queue = this.original.map((i)=>i);
+        }
+
+        var now = this.dateFactory.now();
+        
+        var randomItem = function(lst) {
+            return lst[Math.floor(Math.random()*lst.length)];
+        }
+
+        // show abunai items as a priority
+
+        for(let i in this.abunai){
+            if(this.abunai[i].items.size != 0){
+                if(Math.random() < this.abunai[i].chance){
+                    console.log("Picking random Abunai " + i + " item");        
+                    return randomItem(Array.from(this.abunai[i].items));
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // If we didn't land on an abunai item, show one from the
+        // input queue
+
+        return this.queue.pop()
+
+    }
+
+    isFinished() {
+        return false;
+    }    
+
+    amountLeft() {
+        return Infinity;
+    }
+
+
+}
+
+module.exports = SingleLevelPickStrategy;
